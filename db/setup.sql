d59455c4d7494e289dbf408405ce3c4b30e976ff
James Oliver <joliver.robert@me.com>
2020-05-04 12:23:38 +0100

    Added create-dbs script to package.json

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS york_inquirer;
-DROP DATABASE IF EXISTS york_inquirer;
+DROP DATABASE IF EXISTS york_inquirer_test;
 
-CREATE DATABASE york_inquirer_test;
+CREATE DATABASE york_inquirer;
 CREATE DATABASE york_inquirer_test;
